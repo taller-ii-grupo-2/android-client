bf9aebfc74b196fbb93b0a158a35e58680734aad
augustoarturi <turi.77@hotmail.com>
2019-04-06 13:57:22 -0300

    adding fb login and new interface

--- HypeChatApp/app/src/main/java/com/fiuba/hypechat_app/MainActivity.kt
+++ HypeChatApp/app/src/main/java/com/fiuba/hypechat_app/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.fiuba.hypechat_app
 
+import android.content.Intent
 import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -7,6 +8,8 @@ import android.support.v7.widget.LinearLayoutManager
 import android.widget.Button
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,8 +19,48 @@ class MainActivity : AppCompatActivity() {
 
         val btnDatabase = findViewById(R.id.btnDatabase) as Button
         btnDatabase.setOnClickListener{
-            val txt = findViewById(R.id.txtCheckpoint) as TextView
-            txt.setText("Devolver base de datos")
+            fetchJSON()
+            changeViewToLogin()
         }
     }
+
+    private fun changeViewToLogin() {
+        startActivity(Intent(this,LoginActivity::class.java))
+    }
+
+    fun fetchJSON() {
+         println("Attempt to fetch JSON")
+
+         val url = "https://hypechatgrupo2-app-server.herokuapp.com/android"
+
+         val request = Request.Builder()
+             .url(url)
+             .build()
+
+         val client = OkHttpClient()
+         client.newCall(request).enqueue(object: Callback{
+             override fun onResponse(call: Call, response: Response) {
+                 val body = response.body()?.string()
+                 println(body)
+                 runOnUiThread {
+                     showScreen(body.toString())
+                 }
+
+             }
+             override fun onFailure(call: Call, e: IOException) {
+                 println ("Fail to execute request")
+             }
+         })
+
+
+    }
+
+    fun showScreen(textInput:String){
+        val txt = findViewById(R.id.txtCheckpoint) as TextView
+        txt.setText(textInput)
+
+    }
+
+
+
 }
