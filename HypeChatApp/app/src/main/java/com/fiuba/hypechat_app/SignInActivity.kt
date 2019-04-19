3e44318b6ae8bd3960cd36bdeac3f8c7c107bf89
augustoarturi <turi.77@hotmail.com>
2019-04-19 11:25:19 -0300

    adding forgot password, authentication login with email and pw

--- HypeChatApp/app/src/main/java/com/fiuba/hypechat_app/SignInActivity.kt
+++ HypeChatApp/app/src/main/java/com/fiuba/hypechat_app/SignInActivity.kt
@@ -3,8 +3,10 @@ package com.fiuba.hypechat_app
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.google.firebase.FirebaseApp
+import android.util.Patterns
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_sign_in.*
 
 class SignInActivity : AppCompatActivity() {
@@ -18,18 +20,57 @@ class SignInActivity : AppCompatActivity() {
 
         mAuth = FirebaseAuth.getInstance()
 
-       /* btnSignIn.setOnClickListener{
+        btnSignIn.setOnClickListener{
             confirmSignIn()
-        }*/
+        }
 
-        btnSignUp.setOnClickListener {
+       btnSignUp.setOnClickListener {
             startActivity(Intent(this, SignUpActivity::class.java))
 
+        }
+        btnForgot.setOnClickListener {
+            startActivity(Intent(this, ForgotActivity::class.java))
         }
 
     }
 
     private fun confirmSignIn() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+        if (etEmail.text.toString().isEmpty()){
+            etEmail.error = "Plase enter your email"
+            etEmail.requestFocus()
+            return
+        }
+
+         if (!Patterns.EMAIL_ADDRESS.matcher(etEmail.text.toString()).matches()){
+             etEmail.error = "Plase enter a valid email"
+             etEmail.requestFocus()
+             return
+         }
+
+        if (etPassword.text.toString().isEmpty()){
+            etPassword.error = "Plase enter your password"
+            etPassword.requestFocus()
+            return
+        }
+
+        mAuth.signInWithEmailAndPassword(etEmail.text.toString(), etPassword.text.toString() )
+            .addOnCompleteListener(this) { task->
+                if (task.isSuccessful) {
+                    val user = mAuth.currentUser
+                    updateUI(user)
+
+                } else {
+                    updateUI(null)                }
+            }
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        if (user != null){
+            startActivity(Intent(this, MainActivity::class.java))
+        } else{
+            Toast.makeText(baseContext, "Sign in failed, wrong credentials", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
--- HypeChatApp/app/src/main/java/com/fiuba/hypechat_app/SignUpActivity.kt
+++ HypeChatApp/app/src/main/java/com/fiuba/hypechat_app/SignUpActivity.kt
@@ -3,10 +3,11 @@ package com.fiuba.hypechat_app
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.util.Patterns
 import android.widget.Toast
-import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
@@ -19,43 +20,63 @@ class SignUpActivity : AppCompatActivity() {
         mAuth = FirebaseAuth.getInstance()
 
 
-        btnSignUp.setOnClickListener {
+        signupbtn.setOnClickListener {
             signUpValidation()
 
         }
     }
 
     private fun signUpValidation() {
-        if (etUsername.text.toString().isEmpty()){
-            etUsername.error = "Plase enter your username"
-            etUsername.requestFocus()
+        if (usernamebox.text.toString().isEmpty()){
+            usernamebox.error = "Plase enter your username"
+            usernamebox.requestFocus()
             return
         }
-        if (etEmail.text.toString().isEmpty()){
-            etEmail.error = "Plase enter your email"
-            etEmail.requestFocus()
-            return
-        }
-
-       /* if (Patterns.EMAIL_ADDRESS.matcher(etEmail.text.toString()).matches()){
-            etEmail.error = "Plase enter a valid email"
-            etEmail.requestFocus()
-            return
-        }*/
-
-        if (etPassword.text.toString().isEmpty()){
-            etPassword.error = "Plase enter your password"
-            etPassword.requestFocus()
+        if (emailbox.text.toString().isEmpty()){
+            emailbox.error = "Plase enter your email"
+            emailbox.requestFocus()
             return
         }
 
-        mAuth.createUserWithEmailAndPassword(etEmail.text.toString(), etPassword.text.toString() )
+        if (!Patterns.EMAIL_ADDRESS.matcher(emailbox.text.toString()).matches()){
+            emailbox.error = "Plase enter a valid email"
+            emailbox.requestFocus()
+            return
+        }
+
+        if (passwordbox.text.toString().isEmpty()){
+            passwordbox.error = "Plase enter your password"
+            passwordbox.requestFocus()
+            return
+        }
+
+        mAuth.createUserWithEmailAndPassword(emailbox.text.toString(), passwordbox.text.toString() )
             .addOnCompleteListener(this) { task->
                 if (task.isSuccessful) {
+                    //saveUserToFirebaseDB()
                    startActivity(Intent(this,SignInActivity::class.java))
+                    saveUserToFirebaseDB()
+
+
                 } else {
                     Toast.makeText(baseContext, "Sign up failed", Toast.LENGTH_SHORT).show()
                 }
             }
     }
+
+
+
+    private fun saveUserToFirebaseDB (){
+        val uid = FirebaseAuth.getInstance().uid ?:""
+        val ref = FirebaseDatabase.getInstance().getReference("/users/$uid")
+        val user = User(usernamebox.text.toString(), emailbox.text.toString(), uid)
+        ref.setValue(user)
+            .addOnSuccessListener {
+                Log.d("SignUpActivity", "User added to database")
+
+            }
+    }
 }
+
+
+class User (val username:String, val email: String, val uid:String)
