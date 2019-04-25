572c1dab80c2c67b00d5198fdc789b6ee8c15f6f
Augusto Arturi <arturiaugusto@gmail.com>
2019-04-25 11:30:13 -0300

    initial workspace creation

---
+++ HypeChatApp/app/src/main/java/com/fiuba/hypechat_app/activities/WorkspaceActivity.kt
@@ -0,0 +1,12 @@
+package com.fiuba.hypechat_app.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class WorkspaceActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_workspace)
+    }
+}
